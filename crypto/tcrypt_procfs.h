1f6cefd50e80085d6a42811c63352e02920c0a05
LMAO-armv8 <chaitanya4g9@sasi.ac.in>
2023-05-23 13:48:56 +0530

    crypto: Import OEM Changes
    Signed-off-by: LMAO-armv8 <chaitanya4g9@sasi.ac.in>

--- crypto/tcrypt_procfs.h
+++ crypto/tcrypt_procfs.h
@@ -14,20 +14,15 @@
  * any later version.
  *
  */
-#ifndef _CRYPTO_TCRYPT_H
-#define _CRYPTO_TCRYPT_H
+#ifndef _CRYPTO_TCRYPT_DEBUG_FS_H
+#define _CRYPTO_TCRYPT_DEBUG_FS_H
 
-struct cipher_speed_template {
+struct t_cipher_speed_template {
 	const char *key;
 	unsigned int klen;
 };
 
-struct aead_speed_template {
-	const char *key;
-	unsigned int klen;
-};
-
-struct hash_speed {
+struct t_hash_speed {
 	unsigned int blen;	/* buffer length */
 	unsigned int plen;	/* per-update length */
 	unsigned int klen;	/* key length */
@@ -36,9 +31,9 @@ struct hash_speed {
 /*
  * DES test vectors.
  */
-#define DES3_SPEED_VECTORS	1
+#define T_DES3_SPEED_VECTORS	1
 
-static struct cipher_speed_template des3_speed_template[] = {
+static struct t_cipher_speed_template t_des3_speed_template[] = {
 	{
 		.key	= "\x01\x23\x45\x67\x89\xab\xcd\xef"
 			  "\x55\x55\x55\x55\x55\x55\x55\x55"
@@ -50,30 +45,24 @@ static struct cipher_speed_template des3_speed_template[] = {
 /*
  * Cipher speed tests
  */
-static u8 speed_template_8[] = {8, 0};
-static u8 speed_template_24[] = {24, 0};
-static u8 speed_template_8_16[] = {8, 16, 0};
-static u8 speed_template_8_32[] = {8, 32, 0};
-static u8 speed_template_16_32[] = {16, 32, 0};
-static u8 speed_template_16_24_32[] = {16, 24, 32, 0};
-static u8 speed_template_20_28_36[] = {20, 28, 36, 0};
-static u8 speed_template_32_40_48[] = {32, 40, 48, 0};
-static u8 speed_template_32_48[] = {32, 48, 0};
-static u8 speed_template_32_48_64[] = {32, 48, 64, 0};
-static u8 speed_template_32_64[] = {32, 64, 0};
-static u8 speed_template_32[] = {32, 0};
-
-/*
- * AEAD speed tests
- */
-static u8 aead_speed_template_19[] = {19, 0};
-static u8 aead_speed_template_20[] = {20, 0};
-static u8 aead_speed_template_36[] = {36, 0};
+static u8 t_speed_template_8[] = {8, 0};
+static u8 t_speed_template_24[] = {24, 0};
+static u8 t_speed_template_8_16[] = {8, 16, 0};
+static u8 t_speed_template_8_32[] = {8, 32, 0};
+static u8 t_speed_template_16_32[] = {16, 32, 0};
+static u8 t_speed_template_16_24_32[] = {16, 24, 32, 0};
+static u8 t_speed_template_20_28_36[] = {20, 28, 36, 0};
+static u8 t_speed_template_32_40_48[] = {32, 40, 48, 0};
+static u8 t_speed_template_32_48[] = {32, 48, 0};
+static u8 t_speed_template_32_48_64[] = {32, 48, 64, 0};
+static u8 t_speed_template_32_64[] = {32, 64, 0};
+static u8 t_speed_template_32[] = {32, 0};
 
 /*
  * Digest speed tests
  */
-static struct hash_speed generic_hash_speed_template[] = {
+static struct t_hash_speed t_generic_hash_speed_template[] = {
+#if 0
 	{ .blen = 16,	.plen = 16, },
 	{ .blen = 64,	.plen = 16, },
 	{ .blen = 64,	.plen = 64, },
@@ -96,12 +85,22 @@ static struct hash_speed generic_hash_speed_template[] = {
 	{ .blen = 8192,	.plen = 1024, },
 	{ .blen = 8192,	.plen = 4096, },
 	{ .blen = 8192,	.plen = 8192, },
-
+#else
+	{ .blen = 16,	.plen = 16, },
+	{ .blen = 64,	.plen = 64, },
+	{ .blen = 256,	.plen = 256, },
+	{ .blen = 1024,	.plen = 1024, },
+	{ .blen = 2048,	.plen = 256, },
+	{ .blen = 2048,	.plen = 2048, },
+	{ .blen = 4096,	.plen = 256, },
+	{ .blen = 4096,	.plen = 4096, },
+#endif
 	/* End marker */
 	{  .blen = 0,	.plen = 0, }
 };
 
-static struct hash_speed hash_speed_template_16[] = {
+static struct t_hash_speed t_hash_speed_template_16[] = {
+#if 0
 	{ .blen = 16,	.plen = 16,	.klen = 16, },
 	{ .blen = 64,	.plen = 16,	.klen = 16, },
 	{ .blen = 64,	.plen = 64,	.klen = 16, },
@@ -124,27 +123,19 @@ static struct hash_speed hash_speed_template_16[] = {
 	{ .blen = 8192,	.plen = 1024,	.klen = 16, },
 	{ .blen = 8192,	.plen = 4096,	.klen = 16, },
 	{ .blen = 8192,	.plen = 8192,	.klen = 16, },
+#else
+	{ .blen = 16,	.plen = 16,	.klen = 16, },
+	{ .blen = 64,	.plen = 64,	.klen = 16, },
+	{ .blen = 256,	.plen = 256,	.klen = 16, },
+	{ .blen = 1024,	.plen = 1024,	.klen = 16, },
+	{ .blen = 2048,	.plen = 256,	.klen = 16, },
+	{ .blen = 2048,	.plen = 2048,	.klen = 16, },
+	{ .blen = 4096,	.plen = 256,	.klen = 16, },
+	{ .blen = 4096,	.plen = 4096,	.klen = 16, },
+#endif
 
 	/* End marker */
 	{  .blen = 0,	.plen = 0,	.klen = 0, }
 };
 
-static struct hash_speed poly1305_speed_template[] = {
-	{ .blen = 96,	.plen = 16, },
-	{ .blen = 96,	.plen = 32, },
-	{ .blen = 96,	.plen = 96, },
-	{ .blen = 288,	.plen = 16, },
-	{ .blen = 288,	.plen = 32, },
-	{ .blen = 288,	.plen = 288, },
-	{ .blen = 1056,	.plen = 32, },
-	{ .blen = 1056,	.plen = 1056, },
-	{ .blen = 2080,	.plen = 32, },
-	{ .blen = 2080,	.plen = 2080, },
-	{ .blen = 4128,	.plen = 4128, },
-	{ .blen = 8224,	.plen = 8224, },
-
-	/* End marker */
-	{  .blen = 0,	.plen = 0, }
-};
-
-#endif	/* _CRYPTO_TCRYPT_H */
+#endif	/* _CRYPTO_TCRYPT_DEBUG_FS_H */
